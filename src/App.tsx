0c8306d853c53de43454c364b478ab257e46d144
Solomon-Dzokoto <dzokotosolomon85@gmail.com>
2025-02-17 19:41:25 +0000

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import ContextApi from "./context/ContextApi"
+
+const App = () => {
+  return (
+    <ContextApi>
+      <div>
+        <p>h1</p>
+      </div>
+    </ContextApi>
+  )
+}
+
+export default App
